76f538565fccaa115b280ba0e9bfcb3b4e411a9d
mothibimalebana <mothibi.mm12@gmail.com>
2025-01-15 10:27:36 +0200

    user input will now be saved in state:userResponse

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,23 +1,54 @@
+import { personalDetails } from '../../data/Data.jsx';
+import { useState } from 'react';
+
 //Input component generates a label and a input
 function Input({ title, width, height, required, type }) {
-  function handleSubmit(event) {
-    event.target[0].value = ''; //clear input
-    event.preventDefault(); //prevent form default behaviour
-  }
-
   return (
     <>
-      <div className="inputFormDiv">
+      <label htmlFor={title + type}>{title}</label>
+      <input
+        type={type}
+        height={height}
+        width={width}
+        required={required}
+        id={title + type}
+      ></input>
+    </>
+  );
+}
+
+function Section({ sectionTitle, sectionContent }) {
+  const [userResponse, setUserResponse] = useState('first render');
+
+  function handleSubmit(event) {
+    //get response values:
+    const newUserResponse = []; //array to contain new user response
+    for (let i = 0; i < sectionContent.length; i++) {
+      newUserResponse.push(event.target[i].value);
+      setUserResponse(newUserResponse);
+    }
+    event.preventDefault(); //prevent form default behaviour
+  }
+  console.log(userResponse);
+  return (
+    <>
+      <h3>{sectionTitle}</h3>
+      <div>
         <form action="#" onSubmit={handleSubmit}>
-          <label htmlFor={title + type}>{title}</label>
-          <input
-            type={type}
-            height={height}
-            width={width}
-            required={required}
-            id={title + type}
-          ></input>
-          <input type="submit" value="Submit" />
+          {/* personal details secion */}
+          {sectionContent.map((inputElem) => {
+            return (
+              <Input
+                title={inputElem.title}
+                width={inputElem.width}
+                height={inputElem.height}
+                required={inputElem.required}
+                type={inputElem.type}
+                key={inputElem.key}
+              />
+            );
+          })}
+          <button type="submit">Submit</button>
         </form>
       </div>
     </>
@@ -28,12 +59,9 @@ export default function Form() {
   return (
     <>
       <div>
-        <Input
-          title="Name"
-          width="900"
-          height="180"
-          required={true}
-          type="text"
+        <Section
+          sectionTitle="Personal Details"
+          sectionContent={personalDetails}
         />
       </div>
     </>
--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -1,10 +1,52 @@
 export const personalDetails = [
-  'First Name',
-  'Last Name',
-  'Email',
-  'City',
-  'Phone',
-  'Country',
+  {
+    title: 'First Name',
+    height: 180,
+    width: 900,
+    type: 'text',
+    required: true,
+    key: crypto.randomUUID(),
+  },
+  {
+    title: 'Last Name',
+    height: 180,
+    width: 900,
+    type: 'text',
+    required: true,
+    key: crypto.randomUUID(),
+  },
+  {
+    title: 'Email',
+    height: 180,
+    width: 900,
+    type: 'email',
+    required: true,
+    key: crypto.randomUUID(),
+  },
+  {
+    title: 'City',
+    height: 180,
+    width: 900,
+    type: 'text',
+    required: true,
+    key: crypto.randomUUID(),
+  },
+  {
+    title: 'Phone',
+    height: 180,
+    width: 900,
+    type: 'tel',
+    required: true,
+    key: crypto.randomUUID(),
+  },
+  {
+    title: 'Country',
+    height: 180,
+    width: 900,
+    type: 'text',
+    required: true,
+    key: crypto.randomUUID(),
+  },
 ];
 
 export const proffessionalSummary = ['Proffesional Summary'];
